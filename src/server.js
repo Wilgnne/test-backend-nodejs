8765b559e9ce773bb6f07fe72a333a078eaa1ab2
Wilgnne Khawan <wilgnne.kba@gmail.com>
2020-11-09 10:51:49 -0300

    feat: Connect to mongodb

--- src/server.js
+++ src/server.js
@@ -1,4 +1,11 @@
+const mongoose = require('mongoose');
+
 const app = require('./app');
+const mongoConfig = require('./services/mongoService');
+
+mongoose.connect(mongoConfig.uris, mongoConfig.options)
+  .then(() => console.log('successfully connected to mongodb'))
+  .catch(() => console.log('error connecting to mongodb'));
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('Server running');
