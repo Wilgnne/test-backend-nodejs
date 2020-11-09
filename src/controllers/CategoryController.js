34bf8667f5c096267edf1586a2c4b48e25fa540b
Wilgnne Khawan <wilgnne.kba@gmail.com>
2020-11-09 12:47:46 -0300

    feat: Category controller

---
+++ src/controllers/CategoryController.js
@@ -0,0 +1,32 @@
+const Category = require('../models/Category');
+
+// index, show, store, update, destroy
+
+module.exports = {
+  async index(request, response) {
+    const category = await Category.find();
+
+    return response.json(category);
+  },
+
+  async store(request, response) {
+    const {
+      title,
+    } = request.body;
+
+    const category = await Category.create({
+      title,
+    });
+
+    return response.json(category);
+  },
+
+  async destroy(request, response) {
+    const { id } = request.params;
+
+    const deleted = await Category.deleteOne({ _id: id });
+
+    return response.json(deleted);
+  },
+
+};
