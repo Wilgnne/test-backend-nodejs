9b41d0f2650c5ae48283e19577a88cf482ff8d49
Wilgnne Khawan <wilgnne.kba@gmail.com>
2020-11-09 13:05:19 -0300

    feat: Category routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 
 const ProductController = require('./controllers/ProductController');
+const CategoryController = require('./controllers/CategoryController');
+
 const routes = Router();
 
 routes.get('/prod', ProductController.index);
@@ -8,4 +10,9 @@ routes.post('/prod', ProductController.store);
 routes.patch('/prod/cat/:id', ProductController.updateCategory);
 routes.patch('/prod/:id', ProductController.update);
 routes.delete('/prod/:id', ProductController.destroy);
+
+routes.get('/cat', CategoryController.index);
+routes.post('/cat', CategoryController.store);
+routes.delete('/cat/:id', CategoryController.destroy);
+
 module.exports = routes;
