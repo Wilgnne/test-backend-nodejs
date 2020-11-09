a06f62d4295fc99f905bad150b56f526575d7118
Wilgnne Khawan <wilgnne.kba@gmail.com>
2020-11-09 13:04:47 -0300

    feat: Product routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,11 @@
 const { Router } = require('express');
 
+const ProductController = require('./controllers/ProductController');
 const routes = Router();
 
+routes.get('/prod', ProductController.index);
+routes.post('/prod', ProductController.store);
+routes.patch('/prod/cat/:id', ProductController.updateCategory);
+routes.patch('/prod/:id', ProductController.update);
+routes.delete('/prod/:id', ProductController.destroy);
 module.exports = routes;
